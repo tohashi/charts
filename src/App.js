0bfdaa8b01490e7338c29b497964a3e9f580224c
tohashi <dots64if@gmail.com>
2018-02-14 20:49:56 +0900

    remove home

--- src/App.js
+++ src/App.js
@@ -1,17 +1,25 @@
 // @flow
 
 import React, { Component } from 'react';
-import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
-import Home from './components/Home';
+import { BrowserRouter as Router, Route, Link, Redirect } from 'react-router-dom';
 import RechartsSample from './components/RechartsSample';
 import './App.css';
+
+const routes = [
+  { name: 'Recharts', path: '/recharts', component: RechartsSample }
+]
 
 class App extends Component<void> {
   render() {
     return (
       <Router>
         <div className="App">
-          <Route excact path="/" component={Home} />
+          <header className="App-header">
+            <h1 className="App-title">{routes.find(r => r.path === window.location.pathname).name}</h1>
+          </header>
+          <Route path="/">
+            <Redirect to={{ pathname: '/recharts' }} />
+          </Route>
           <Route path="/recharts" component={RechartsSample} />
         </div>
       </Router>
--- src/components/Home.js
+++
@@ -1,9 +0,0 @@
-// @flow
-
-import React, { Component } from 'react';
-
-export default function Home() {
-  return (
-    <div>home</div>
-  );
-}
