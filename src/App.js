c03b8079dfa5384e6acdf8f9cfa79954026e64bf
tohashi <dots64if@gmail.com>
2018-02-14 19:46:00 +0900

    react-router-dom

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
 // @flow
 
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 
-class App extends Component<void, void> {
+class App extends Component<void> {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+      <Router>
+        <Route excact path="/" component={Home} />
+      </Router>
     );
   }
+}
+
+function Home() {
+  return (
+    <div>home</div>
+  );
 }
 
 export default App;
